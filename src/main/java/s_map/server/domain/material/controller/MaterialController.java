320f5d736293c96044d9adf013e1b65e149f2929
BAE <bjh4966@naver.com>
2026-05-15 07:57:01 +0900

    fix: 자재 전체 수정 API를 PUT으로 변경

--- src/main/java/s_map/server/domain/material/controller/MaterialController.java
+++ src/main/java/s_map/server/domain/material/controller/MaterialController.java
@@ -15,11 +15,10 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -63,7 +62,7 @@ public class MaterialController {
     }
 
     @Operation(summary = "자재 정보 수정")
-    @PatchMapping("/{materialId}")
+    @PutMapping("/{materialId}")
     public BaseResponse<MaterialDetailResponse> updateMaterial(
             @PathVariable Long materialId,
             @Valid @RequestBody MaterialUpdateRequest request
